a4c7dfb8523fdf272538d6c438d152df869d06b8
Stephan Sure <stephan.sure@gmail.com>
2024-05-15 18:05:03 +0000

    Add ListTemplate class

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ListTemplate(models.Model):
+    list_owner = models.ForeignKey(User, on_delete=models.SET_NULL, 
+    null=True, blank=True, related_name='owned_lists')
+    list_title = models.CharField(max_length=200)
+   
+    def __str__(self):
+        return self.list_title
